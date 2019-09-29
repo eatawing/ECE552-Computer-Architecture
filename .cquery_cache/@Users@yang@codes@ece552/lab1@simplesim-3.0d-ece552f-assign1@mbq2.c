74d946d707cb5f400b8a269769cd04d53b3b05d9
eatawing <yli191096@gmail.com>
2019-09-28 22:49:26 -0400

    mbq2

---
+++ .cquery_cache/@Users@yang@codes@ece552/lab1@simplesim-3.0d-ece552f-assign1@mbq2.c
@@ -0,0 +1,16 @@
+int main(int argc, char **argv) {
+    int i;
+    for (i = 0; i < 10000; ++i) {
+        __asm__(
+            "addi $1, $0, 1\n\t"
+            "add $2, $0, $1\n\t" // 2-cycle stall()
+            "sub $3, $1, $0\n\t" // no stall
+            "addi $4, $0, 4\n\t"
+            "add $5, $1, $2\n\t"
+            "add $6, $4, $0\n\t" // 1-cycle stall
+            "sw $6, 0($3)\n\t" // no stall
+            "lw $5, 0($6)\n\t" // 2-cycle stall
+            // 5 cycles stall in total
+        );
+    }
+}
---
+++ lab1/simplesim-3.0d-ece552f-assign1/mbq2.c
@@ -0,0 +1,14 @@
+int main(int argc, char **argv) {
+    int i;
+    for (i = 0; i < 10000; ++i) {
+        __asm__(
+            "addi $1, $0, 1\n\t"
+            "add $2, $0, $1\n\t" // 1-cycle stall
+            "addi $3, $0, 3\n\t"
+            "lw $2, 0($1)\n\t"
+            "add $4, $2, $0\n\t" // 2-cycle stall
+            "add $5, $1, $2\n\t"
+            "sw $5, 0($6)" // no stall
+        );
+    }
+}
