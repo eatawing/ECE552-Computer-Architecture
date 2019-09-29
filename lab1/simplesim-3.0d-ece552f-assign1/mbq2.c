8113caed2426ea23afa10529dab53806ac75d856
eatawing <yli191096@gmail.com>
2019-09-28 23:23:16 -0400

    Update mbq2.c

--- lab1/simplesim-3.0d-ece552f-assign1/mbq2.c
+++ lab1/simplesim-3.0d-ece552f-assign1/mbq2.c
@@ -4,13 +4,10 @@ int main(int argc, char **argv) {
         __asm__(
             "addi $1, $0, 1\n\t"
             "add $2, $0, $1\n\t" // 1-cycle stall
-            "addi $3, $0, 3\n\t"
-            "lw $2, 0($1)\n\t"
-            "add $4, $2, $0\n\t" // 2-cycle stall
+            "lw $3, 0($1)\n\t"
+            "add $4, $3, $0\n\t" // 2-cycle stall
             "add $5, $1, $2\n\t"
             "sw $5, 0($6)\n\t" // no stall
-            "addi $7, $0, 7\n\t"
-            "sub $8, $7, $0\n\t" // 1-cycle stall
         );
     }
 }
