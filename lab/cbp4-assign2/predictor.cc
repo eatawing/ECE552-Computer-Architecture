4bfacd1499ae90f3554fa1bca3b00fb8164ae4e4
eatawing <yli191096@gmail.com>
2019-10-14 01:53:30 -0400

    upda

--- lab/cbp4-assign2/predictor.cc
+++ lab/cbp4-assign2/predictor.cc
@@ -76,17 +76,260 @@ void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 bra
 /////////////////////////////////////////////////////////////
 // openend
 /////////////////////////////////////////////////////////////
+#define TAGE_TABLE_NUM 12
+#define BASE_PREDICTOR_SIZE 8192
+#define GHR_LENGTH 480
+
+typedef struct tagePredictor {
+  uint8_t pred;
+  UINT32 tag;
+  uint8_t u;
+} tagePredictor_t;
+
+typedef struct prediction {
+  uint8_t index;
+  uint8_t tableNum;
+  bool dir;
+} prediction_t;
+
+typedef struct fold {
+  UINT32 originLength;
+  UINT32 targetLength;
+  UINT32 history;
+
+  void updateHistory() {
+    history = history << 1 + GHR[0];
+    history ^= ((history & (1 << targetLength)) >> targetLength);
+    history ^= GHR[originLength] << (originLength % targetLength);
+    history &= 1 << targetLength - 1;
+  }
+} fold_t;
+
+uint8_t *basePredictor;
+tagePredictor_t **tageTables;
+bitset<480> GHR;
+int PHR;
+
+prediction_t provider;
+prediction_t altpred;
+
+uint8_t use_alt_on_na;
+UINT32 branchCounter;
+
+UINT32 tageIndexHash[TAGE_TABLE_NUM];
+UINT32 tageTagHash[TAGE_TABLE_NUM];
+
+fold_t indexFold[TAGE_TABLE_NUM];
+fold_t tagFold[2][TAGE_TABLE_NUM];
+
+UINT32 tageTable_tagWidth[TAGE_TABLE_NUM] = {15, 13, 12, 12, 12, 11, 10, 10, 10, 9, 9, 7};
+UINT32 tageTable_entryNum[TAGE_TABLE_NUM] = {128, 128, 256, 512, 512, 1024, 1024, 2048, 1024, 1024, 1024};
+UINT32 tageTable_historyLength[TAGE_TABLE_NUM] = {479, 354, 230, 155, 105, 69, 55, 35, 25, 18, 10, 6};
+
+
+UINT32 hash_tag(UINT32 PC, bitset<GHR_LENGTH> history, UINT32 length) { 
+  return 0;
+}
+
+UINT32 hash_index(UINT32 PC, bitset<GHR_LENGTH> history, UINT32 length) { 
+  return 0;
+}
 
 void InitPredictor_openend() {
+  GHR.reset();
+  PHR = 0;
 
+  basePredictor = new uint8_t[BASE_PREDICTOR_SIZE];
+  for (int i = 0; i < BASE_PREDICTOR_SIZE; i++) {
+    basePredictor[i] = 0b10;
+  }
+  
+  tageTables = new tagePredictor_t*[TAGE_TABLE_NUM];
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    UINT32 entryNum = tageTable_entryNum[i];
+    tageTables[i] = new tagePredictor_t[entryNum];
+    for (int j = 0; j < entryNum; j++) {
+      tageTables[i][j].pred = 0;
+      tageTables[i][j].tag = 0; 
+      tageTables[i][j].u = 0;
+    }
+  }
+
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    indexFold[i].history = 0;
+    indexFold[i].originalLength = tageTable_historyLength[i];
+    indexFold[i].targetLength = tageTable_tagWidth[i];
+
+    tageIndexHash[i] = 0;
+    tageTagHash[i] = 0;
+  }
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < TAGE_TABLE_NUM; j++) {
+      tagFold[i][j].history = 0;
+      tagFold[i][j].originalLength = tageTable_historyLength[j];
+      if (i == 0) {
+        tagFold[i][j].targetLength = tageTable_tagWidth[j];
+      } else {
+        tagFold[i][j].targetLength = tageTable_tagWidth[j] - 1;
+      }
+    }
+  }
+
+  provider.tableNum = -1;
+  altpred.tableNum = -1;
+  
+  use_alt_on_na = 0;
+  branchCounter = 0;
 }
 
 bool GetPrediction_openend(UINT32 PC) {
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    tageTagHash[i] = PC ^ tagFold[0][i].history ^ (tagFold[1][i].history << 1);
+    tageTagHash[i] &= 1 << tageTable_tagWidth[i] - 1;
+  }
 
-  return TAKEN;
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    tageIndexHash[i] = PC ^ (PC >> (tageTable_tagWidth[i] - i)) ^ indexFold[i].history ^ PHR;
+    tageIndexHash[i] &= 1 << tageTable_tagWidth[i] - 1;
+  }
+
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    if (tageTables[i][tageIndex].tag == hash_tag(PC, GHR, tageTable_historyLength[i])) {
+      provider.index = tageIndex;
+      provider.tableNum = i;
+      break;
+    }
+  }
+
+  for (int i = provider.tableNum + 1; i < TAGE_TABLE_NUM; i++) {
+    UINT32 tageIndex = hash_index(PC, GHR, tageTable_historyLength[i]);
+
+    if (tageTables[i][tageIndex].tag == hash_tag(PC, GHR, tageTable_historyLength[i])) {
+      altpred.index = tageIndex;
+      altpred.tableNum = i;
+      break;
+    }
+  }
+
+  if (provider.tableNum >= 0) {
+      if (altpred.tableNum >= 0) {
+        altpred.dir = tageTables[altpred.tableNum][altpred.index].pred > 0b011 ? TAKEN : NOT_TAKEN;
+      } else {
+        altpred.dir = basePredictor[PC % BASE_PREDICTOR_SIZE] > 0b01 ? TAKEN : NOT_TAKEN;
+      }
+
+      if (tageTables[provider.tableNum][provider.index].pred != 0b011 && tageTables[provider.tableNum][provider.index].pred != 0b100 && 
+          tageTables[provider.tableNum][provider.index].u != 0 && use_alt_on_na > 0) {
+        provider.dir = tageTables[provider.tableNum][provider.index].pred > 0b011 ? TAKEN : NOT_TAKEN;
+        return provider.dir;
+      }
+  } else {
+    altpred.dir = basePredictor[PC % BASE_PREDICTOR_SIZE] > 0b01 ? TAKEN : NOT_TAKEN;
+    return altpred.dir;
+  }
 }
 
 void UpdatePredictor_openend(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
+  if (provider.tableNum >= 0) {
+    uint8_t primePrediction = tageTables[provider.tableNum][provider.index].pred;
+    if (resolveDir && primePrediction < 0b111) {
+      tageTables[provider.tableNum][provider.index].pred++;
+    } else if (!resolveDir && primePrediction > 0b000) {
+      tageTables[provider.tableNum][provider.index].pred--;
+    }
 
+    // useful bit is 0
+    if (tageTables[provider.tableNum][provider.index].u == 0 && 
+        (primePrediction == 0b011 || primePrediction == 0b100)) {
+      if (provider.dir == resolveDir && altpred.dir != resolveDir && use_alt_on_na > -8) {
+        use_alt_on_na--;
+      } else if (provider.dir != resolveDir && altpred.dir == resolveDir && use_alt_on_na < 8) {
+        use_alt_on_na++;
+      }
+    }
+
+    if (altpred.tableNum >= 0) {
+      uint8_t altPrediction = tageTables[altpred.tableNum][altpred.index].pred;
+      if (resolveDir && altPrediction < 0b111) {
+        tageTables[altpred.tableNum][altpred.index].pred++;
+      } else if (resolveDir && altPrediction > 0b000) {
+        tageTables[altpred.tableNum][altpred.index].pred--;
+      }
+    }
+
+    if (resolveDir != predDir) {
+      bool vacancy = false;
+      for (int i = 0; i < provider.tableNum; i++) {
+        if (tageTables[i][tageIndexHash[i]].u == 0) {
+          vacancy = true;
+          break;
+        }
+      }
+      
+      if (!vacancy) {
+        for (int i = 0; i < provider.tableNum; i++) {
+          tageTables[i][tageIndexHash[i]].u--;
+        }
+      } else {
+        for (int i = provider.tableNum - 1; i >= 0; i--) {
+          if (tageTables[i][tageIndexHash[i]].u == 0 && rand() % 2) {
+            tageTables[i][tageIndexHash[i]].pred = resolveDir ? 0b100 : 0b011;
+            tageTables[i][tageIndexHash[i]].tag = tageTagHash[i];
+            tageTables[i][tageIndexHash[i]].u = 0;
+
+            break;
+          }
+        }
+      }
+    }
+
+    if (predDir != altpred.dir) {
+      if (predDir == resolveDir && tageTables[provider.tableNum][provider.index].u < 0b11) {
+        tageTables[provider.tableNum][provider.index].u++;
+      } else if (predDir != resolveDir && tageTables[provider.tableNum][provider.index].u > 0b00) {
+        tageTables[provider.tableNum][provider.index].u--;
+      }
+    }
+  } else {
+    // update base predictor
+    UINT32 basePredictorIndex = PC % BASE_PREDICTOR_SIZE;
+    if (resolveDir && basePredictor[basePredictorIndex] < 0b11) {
+      basePredictor[basePredictorIndex]++;
+    } else if (!resolveDir && basePredictor[basePredictorIndex] > 0b00) {
+      basePredictor[basePredictorIndex]--;
+    }
+  }
+
+  branchCounter++;
+  if (branchCounter == 256*1024) {
+    // reset MSB
+    for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+      for (int j = 0; j < tageTable_entryNum[i]; j++) {
+        tageTables[i][j].u &= 0b01
+      }
+    }
+  } else if (branchCounter == 512 * 1024) {
+    // reset LSB
+    branchCounter = 0; 
+    for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+      for (int j = 0; j < tageTable_entryNum[i]; j++) {
+        tageTables[i][j].u &= 0b10
+      }
+    }
+  }
+
+  GHT = GHT << 1;
+  GHR.set(0, resolveDir);
+
+  for (int i = 0; i < TAGE_TABLE_NUM; i++) {
+    tagFold[0][i].updateHistory();
+    tagFold[1][i].updateHistory();
+    indexFold[i].updateHistory();
+  }
+
+  PHR = PHR << 1;
+  PHR = PHR & (1 << 16 - 1);
+  PHR = PHR + (PC & 0b1);
 }
 
